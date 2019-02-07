6bc7e03d2a51a95aa3433f553c09d07bd4ed8a97
jfearnow <jfearnow79@gmail.com>
2019-02-07 14:46:48 -0500

    created function in game: Jon

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -2,16 +2,22 @@ import random
 answer = random.randint(1,11)
 guess = input('Guess a Number ')
 guess = int(guess)
-while True:
+
+def number_guessing_game(guess, answer):
+    # guess = input('Guess a Number ')
+    # guess = int(guess)
     if guess < answer:
         guess = input('Try Again number was to low ')
         guess = int(guess)
+        number_guessing_game(guess, answer)
     elif guess > answer:
-          guess = input('Try again number was to high ')
-          guess = int(guess)
+        guess = input('Try again number was to high ')
+        guess = int(guess)
+        number_guessing_game(guess, answer)
     else:
         print('Youre Right')
-        break
+number_guessing_game(guess, answer)
+
 
 
 
