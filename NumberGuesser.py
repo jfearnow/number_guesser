0da481d113a64bf068f2e820f984277bfa456ab3
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-02-07 14:57:42 -0500

    Update game

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -4,18 +4,21 @@ guess = input('Guess a Number ')
 guess = int(guess)
 
 def number_guessing_game(guess, answer):
-    # guess = input('Guess a Number ')
-    # guess = int(guess)
     if guess < answer:
-        guess = input('Try Again number was to low ')
+        guess = input('Try Again number was to low \n')
         guess = int(guess)
         number_guessing_game(guess, answer)
     elif guess > answer:
-        guess = input('Try again number was to high ')
+        guess = input('Try again number was to high \n')
         guess = int(guess)
         number_guessing_game(guess, answer)
     else:
         print('Youre Right')
+        play_again_option = input('Enter 1 to play again, otherwise exit program \n')
+        play_again_option = int(play_again_option)
+        if play_again_option == 1:
+            new_answer = random.randint(2,11)
+            number_guessing_game(guess, new_answer)
 number_guessing_game(guess, answer)
 
 
