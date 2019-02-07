18f46dce55cf18f76d94072345b6caf8c6a8e40c
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-02-07 14:28:49 -0500

    Updated number guessing game

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -1,14 +1,17 @@
-answer = 10
+import random
+answer = random.randint(1,11)
 guess = input('Guess a Number ')
 guess = int(guess)
 while True:
     if guess < answer:
         guess = input('Try Again number was to low ')
         guess = int(guess)
-    elif guess == answer:
-        print('Youre Right')
-        break
     elif guess > answer:
           guess = input('Try again number was to high ')
           guess = int(guess)
+    else:
+        print('Youre Right')
+        break
+
+
 
