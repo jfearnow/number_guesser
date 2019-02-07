c7a1df65096a1bcb2793a93523fdee4cf57ea324
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-02-07 16:12:21 -0500

    Added while loop to prompt user for new guess number

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -1,15 +1,18 @@
 import random
 answer = random.randint(1,11)
-guess = input('Guess a Number ')
+guess = input('Guess a Number \n')
 guess = int(guess)
 
 def number_guessing_game(guess, answer):
+    while guess == 0:
+        guess = input('Guess a Number \n')
+        guess = int(guess)
     if guess < answer:
-        guess = input('Try Again number was to low \n')
+        guess = input('Try Again number was too low \n')
         guess = int(guess)
         number_guessing_game(guess, answer)
     elif guess > answer:
-        guess = input('Try again number was to high \n')
+        guess = input('Try again number was too high \n')
         guess = int(guess)
         number_guessing_game(guess, answer)
     else:
@@ -17,10 +20,6 @@ def number_guessing_game(guess, answer):
         play_again_option = input('Enter 1 to play again, otherwise exit program \n')
         play_again_option = int(play_again_option)
         if play_again_option == 1:
-            new_answer = random.randint(2,11)
-            number_guessing_game(guess, new_answer)
+            new_answer = random.randint(1,11)
+            number_guessing_game(0, new_answer)
 number_guessing_game(guess, answer)
-
-
-
-
