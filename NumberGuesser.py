260a6ce3a748c5a76fb98b561260ea9bf8242875
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-02-07 16:16:57 -0500

    Refactored code to make it shorter

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -1,24 +1,19 @@
 import random
 answer = random.randint(1,11)
-guess = input('Guess a Number \n')
-guess = int(guess)
+guess = 0
 
 def number_guessing_game(guess, answer):
     while guess == 0:
-        guess = input('Guess a Number \n')
-        guess = int(guess)
+        guess = int(input('Guess a Number \n'))
     if guess < answer:
-        guess = input('Try Again number was too low \n')
-        guess = int(guess)
+        guess = int(input('Try Again number was too low \n'))
         number_guessing_game(guess, answer)
     elif guess > answer:
-        guess = input('Try again number was too high \n')
-        guess = int(guess)
+        guess = int(input('Try again number was too high \n'))
         number_guessing_game(guess, answer)
     else:
         print("You're Right")
-        play_again_option = input('Enter 1 to play again, otherwise exit program \n')
-        play_again_option = int(play_again_option)
+        play_again_option = int(input('Enter 1 to play again, otherwise exit program \n'))
         if play_again_option == 1:
             new_answer = random.randint(1,11)
             number_guessing_game(0, new_answer)
